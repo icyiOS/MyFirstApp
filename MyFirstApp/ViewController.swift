6476164ce596ae8218efabd03cc8366b917e9821
李超逸 <icylydia@gmail.com>
2018-01-26 18:40:20 +0900

    till Chapter 4

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 50
+    var targetValue: Int = 0
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,17 +28,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(_ sender: Any) {
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
+        
         let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my first app!",
+                                      message: message,
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Awesome",
+        let action = UIAlertAction(title: "OK",
                                    style: .default,
                                    handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
 
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        currentValue = lroundf(sender.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
 }
 
